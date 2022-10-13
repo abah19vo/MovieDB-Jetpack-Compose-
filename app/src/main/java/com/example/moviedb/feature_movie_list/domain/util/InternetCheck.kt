f039279b1c69fbead40e8c00b5c275521943fa4c
ahmad-adnan.abdullah <ahmad-adnan.abdullah@sogeti.com>
2022-10-13 08:20:09 +0200

    added some of the required files and classes

---
+++ app/src/main/java/com/example/moviedb/feature_movie_list/domain/util/InternetCheck.kt
@@ -0,0 +1,4 @@
+package com.example.moviedb.feature_movie_list.domain.util
+
+class InternetCheck {
+}
---
+++ app/src/main/java/com/example/moviedb/feature_movie_list/presentation/movies/MovieListViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.moviedb.feature_movie_list.presentation.movies
+
+class MovieListViewModel {
+}
