2349517acc452d4aa0f16ef90530f34fd95cb8e0
ahmad-adnan.abdullah <ahmad-adnan.abdullah@sogeti.com>
2022-10-21 08:43:38 +0200

    created interface to repository for more testble code

--- app/src/main/java/com/example/moviedb/feature_movie_list/data/repository/IMovieRepository.kt
+++ app/src/main/java/com/example/moviedb/feature_movie_list/data/repository/IMovieRepository.kt
@@ -2,9 +2,9 @@ package com.example.moviedb.feature_movie_list.data.repository
 
 import com.example.moviedb.feature_movie_list.domain.model.MovieDetails
 import com.example.moviedb.feature_movie_list.domain.model.MovieList
-import retrofit2.Response
+import com.example.moviedb.feature_movie_list.presentation.util.Resource
 
 interface IMovieRepository {
-    suspend fun getMovieList(pageNumber:Int = 1) : Response<MovieList>
-    suspend fun getMovieById(movieId : Int) : Response<MovieDetails>
+    suspend fun getMovieList(pageNumber:Int = 1) : Resource<MovieList>
+    suspend fun getMovieById(movieId : Int) : Resource<MovieDetails>
 }
--- app/src/main/java/com/example/moviedb/feature_movie_list/data/repository/MovieRepository.kt
+++ app/src/main/java/com/example/moviedb/feature_movie_list/data/repository/MovieRepository.kt
@@ -1,10 +1,29 @@
 package com.example.moviedb.feature_movie_list.data.repository
 
 import com.example.moviedb.feature_movie_list.data.api_services.MovieServiceInstance
+import com.example.moviedb.feature_movie_list.domain.model.MovieDetails
+import com.example.moviedb.feature_movie_list.domain.model.MovieList
+import com.example.moviedb.feature_movie_list.domain.util.CustomException
+import com.example.moviedb.feature_movie_list.presentation.util.Resource
 
 class MovieRepository : IMovieRepository{
-    override suspend fun getMovieList(pageNumber:Int)
-            = MovieServiceInstance.api.getMovieList(pageNumber = pageNumber)
-    override suspend fun getMovieById(movieId : Int)
-            =  MovieServiceInstance.api.getMovieDetails(movieId)
+    override suspend fun getMovieList(pageNumber:Int) : Resource<MovieList>{
+        val response = MovieServiceInstance.api.getMovieList(pageNumber = pageNumber)
+        if(!response.isSuccessful) throw CustomException.UnSuccessfulRequest()
+        else response.body()?.let { movieList->
+            if(movieList.results.isEmpty()) throw CustomException.NoContent()
+            return Resource.Success(movieList)
+        }
+            ?:throw CustomException.UnexpectedError()
+    }
+    override suspend fun getMovieById(movieId : Int): Resource<MovieDetails>{
+        val response = MovieServiceInstance.api.getMovieDetails(movieId)
+        if(!response.isSuccessful) throw CustomException.UnSuccessfulRequest(response.message())
+        if(response.body() == null) throw CustomException.NoContent()
+        else response.body()?.let { movie ->
+            return Resource.Success(movie)
+        }?: throw CustomException.UnexpectedError()
+
+
+    }
 }
